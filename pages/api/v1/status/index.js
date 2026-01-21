e6f24cfd7587503dfe3ba2bed7bbf70159176eb6
Lemuel <lemuel.czara@gmail.com>
2026-01-21 01:43:15 +0000

    create database connection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function handler(req, res) {
+import database from "../../../../infra/database";
+
+export default async function handler(req, res) {
+  const result = await database.query("SELECT 1 + 1 AS SUM;");
+  console.log(result.rows);
   res.status(200).json({ status: "ok" });
 }
