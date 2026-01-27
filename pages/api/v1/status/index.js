562c836dde588725520113f850550f8145bf3c0b
Lemuel <lemuel.czara@gmail.com>
2026-01-27 02:12:14 +0000

    endpoint-status-opened-connections

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,27 @@
 import database from "infra/database";
 
 export default async function handler(req, res) {
-  const result = await database.query("SELECT 1 + 1 AS SUM;");
-  console.log(result.rows);
-  res.status(200).json({ status: "ok" });
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = dbMaxConnectionsResult.rows[0].max_connections;
+
+  const dbOpenedConnectionsResult = await database.query(
+    "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  const dbOpenedConnectionsValue = dbOpenedConnectionsResult.rows[0].count;
+
+  const updatedAt = new Date().toISOString();
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionsValue),
+        used_connections: dbOpenedConnectionsValue,
+      },
+    },
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,19 @@
 test("GET to /api/v1/status returns 200 and the correct response body", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
-
   expect(res.status).toBe(200);
+
+  const responseBody = await res.json();
+
+  const parsedDate = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody).toEqual({
+    updated_at: parsedDate,
+    dependencies: {
+      database: {
+        version: "18.1",
+        max_connections: 100,
+        used_connections: 1,
+      },
+    },
+  });
 });
